21c0968dda79df313b43b8ad461cf56a228838ed
zoldarcrux <zoldarcrux@gmail.com>
2025-10-12 15:58:56 -0300

    Update callback.py

--- api/oauth/callback.py
+++ api/oauth/callback.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
-
 app = FastAPI()
 
-@app.get("/api/oauth/callback")
+@app.get("/")
 async def oauth_callback(request: Request):
     code = request.query_params.get("code", "")
     return PlainTextResponse(f"Auth code recibido: {code}")
